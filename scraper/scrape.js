6e59c1a7f908adf310ebcbced9b5554c018a9bec
Declan Cook <DeclanCook44@gmail.com>
2014-02-19 23:04:54 +0000

    rename scaper files

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,41 +1,15 @@
-var fs = require('fs'),
-    movies = JSON.parse(fs.readFileSync('../movies.json')),
-    cp = require('child_process'),
-    running = 0,
-    completed = 0,
-    alreadyCompleted = 0;
+var page = require('webpage').create(),
+    args = require('system').args;
 
-for(j = 0; j < 40; j++){
-    spawnScraper()
-}
-
-
-function spawnScraper(){
-    var i = running++
-    if(running > movies.length){
-        return
+page.open('http://www.imdb.com' + args[1], function (status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+    } else {
+        var p = page.evaluate(function () {
+            $('div[itemprop="description"]').children().children().remove()
+            return $('div[itemprop="description"]').text()
+        });
+        console.log(p);
     }
-    if(movies[i].desc){
-        alreadyCompleted++
-        spawnScraper()
-        return
-    }
-    console.log('Spawning scraper for ', i)
-    var ps = cp.spawn('phantomjs', ['scrap.js', movies[i].url])
-    ps.stdout.setEncoding('utf8')
-    ps.stdout.on('data', function(data){
-        movies[i].desc = data.replace(/\n/g, '').trim()
-        console.log("i: ",i , "data: ", data)
-    })
-    ps.on('exit', function(){
-        completed++
-        console.log('Completed:' + completed)
-        if(completed < movies.length - alreadyCompleted){
-            spawnScraper()
-        } else {
-            fs.writeFile('../movies.json', JSON.stringify(movies), function(err){
-                console.log('Saved')
-            })
-        }
-    })
-}
+    phantom.exit();
+});
