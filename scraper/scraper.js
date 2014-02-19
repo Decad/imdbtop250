fe4039b5b9ee30f584f1cf1fe0fbabeb8eeda903
Declan Cook <DeclanCook44@gmail.com>
2014-02-19 23:22:35 +0000

    Add scraper.js

---
+++ scraper/scraper.js
@@ -0,0 +1,41 @@
+var fs = require('fs'),
+    movies = JSON.parse(fs.readFileSync('../movies.json')),
+    cp = require('child_process'),
+    running = 0,
+    completed = 0,
+    alreadyCompleted = 0;
+
+for(j = 0; j < 40; j++){
+    spawnScraper()
+}
+
+
+function spawnScraper(){
+    var i = running++
+    if(running > movies.length){
+        return
+    }
+    if(movies[i].desc){
+        alreadyCompleted++
+        spawnScraper()
+        return
+    }
+    console.log('Spawning scraper for ', i)
+    var ps = cp.spawn('phantomjs', ['scrape.js', movies[i].url])
+    ps.stdout.setEncoding('utf8')
+    ps.stdout.on('data', function(data){
+        movies[i].desc = data.replace(/\n/g, '').trim()
+        console.log("i: ",i , "data: ", data)
+    })
+    ps.on('exit', function(){
+        completed++
+        console.log('Completed:' + completed)
+        if(completed < movies.length - alreadyCompleted){
+            spawnScraper()
+        } else {
+            fs.writeFile('../movies.json', JSON.stringify(movies), function(err){
+                console.log('Saved')
+            })
+        }
+    })
+}
